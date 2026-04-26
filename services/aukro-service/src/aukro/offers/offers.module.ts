d5332279674283fc588e7dd3ccb3679cc9c4da7b
speakASAP <stashok@speakasap.com>
2026-04-26 06:09:39 +0200

    Refactor Dockerfile and update Prisma schema to enhance build process and add Heureka models

--- services/aukro-service/src/aukro/offers/offers.module.ts
+++ services/aukro-service/src/aukro/offers/offers.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OffersController } from './offers.controller';
 import { OffersService } from './offers.service';
-import { PrismaModule, ClientsModule } from '@heureka/shared';
+import { PrismaModule, ClientsModule, AuthModule } from '@heureka/shared';
 
 @Module({
-  imports: [PrismaModule, ClientsModule],
+  imports: [PrismaModule, ClientsModule, AuthModule],
   controllers: [OffersController],
   providers: [OffersService],
   exports: [OffersService],
